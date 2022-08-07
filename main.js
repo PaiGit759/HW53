27d37afd56ffb502d1e1a668008d3292e5c7d17d
PaiGit759 <107637697+PaiGit759@users.noreply.github.com>
2022-08-07 12:50:00 +0200

    20220807 12:49

--- main.js
+++ main.js
@@ -1,20 +1,61 @@
 let button_press = false;
 
-var input_string;
+//var input_date;
+var number_date;
 
 var result = "";
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+var d = new Date();
+
 
 function button_onclick()
 {
  
-    input_string = document.getElementById("fild_for_input").value;
+    input_date = new Date(document.getElementById("fild_for_input").value);
+    number_date = input_date.getDay();
 
     document.getElementById("exsamp").style.background = "#A0bF";
     document.getElementById("exsamp").style.color = "#FFFF";
-    document.getElementById("exsamp").innerText = "";
+    document.getElementById("exsamp").innerText = number_date;
 
-    length_string =  input_string.length;
 
+switch (number_date) {
+
+    case  0 : {
+    result = "This is a Sunday";
+    break;
+}
+    case  1 : {
+        result = "This is a Monday";
+        break;
+    }
+    case  2 : {
+        result = "This is a Tuesday";
+        break;
+    }
+    case  3 : {
+        result = "This is a Wednesday";
+        break;
+    }
+    case  4 : {
+        result = "This is a Thursday";
+        break;
+    }
+    case  5 : {
+        result = "This is a Friday";
+        break;
+    }
+    case  6 : {
+        result = "This is a Saturday";
+        break;
+    }
+
+}
+
+
+/*
 
 if (length_string < 5) {
     result = "This is a tiny string";
@@ -34,6 +75,10 @@ else if ((length_string >= 15)&&(length_string < 30)){
 else if (length_string >= 30) {
     result = "This is a gigantic string";
 };
+ */
+
+//result = number_date;
+
 
 document.getElementById("exsamp").innerText = result;
 }
